5a830c868cb05f2121aff07f35388832e9d7bdfd
Alejandro Montoya <alejomontoya639@gmail.com>
2025-12-16 15:18:23 -0500

    primer commit

---
+++ src/main/java/com/example/SuraExpert/SuraExpertApplication.java
@@ -0,0 +1,13 @@
+package com.example.SuraExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuraExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuraExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SuraExpert/SuraExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SuraExpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuraExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
